c1ac85b653c1cc1dbf5ba93f6fd293afe23b90d1
adamfrsd-02 <adamfrdsid@gmail.com>
2022-07-13 14:41:12 +0700

    add logic for relay

--- routes/api/lakes.js
+++ routes/api/lakes.js
@@ -6,35 +6,9 @@ module.exports = (app) => {
         const api = "https://testing-z.herokuapp.com"
 
         async function apiCall() {
-            try {
-                const {
-                    data: controls
-                } = await axios.get(`${api}/api/controls/625cfa9898b4120cea428ac4`);
-                const {
-                    data: phStatus
-                } = await axios.get(`${api}/api/sensors/device/ph_sensor`);
-                const {
-                    data: nutriStatus
-                } = await axios.get(`${api}/api/sensors/device/nutrition_sensor`);
-                const {
-                    data: waterHeightStatus
-                } = await axios.get(`${api}/api/sensors/device/water_height_sensor`);
-                res.json({
-                    controls,
-                    phStatus,
-                    nutriStatus,
-                    waterHeightStatus
-                });
-
-
-
-            } catch (error) {
-                console.log(error);
-            }
-
             axios.get(`${api}/api/sensors/device/ph_sensor`)
                 .then(function (res) {
-                    const ph = response.data[0].data;
+                    const ph = res.data[0].data;
 
                     //handle ph down
                     if (ph <= 6.2) {
@@ -72,9 +46,38 @@ module.exports = (app) => {
                 }).catch(function (err) {
                     console.log(err);
                 });
+
+            //fetch api
+            try {
+                const {
+                    data: controls
+                } = await axios.get(`${api}/api/controls/625cfa9898b4120cea428ac4`);
+                const {
+                    data: phStatus
+                } = await axios.get(`${api}/api/sensors/device/ph_sensor`);
+                const {
+                    data: nutriStatus
+                } = await axios.get(`${api}/api/sensors/device/nutrition_sensor`);
+                const {
+                    data: waterHeightStatus
+                } = await axios.get(`${api}/api/sensors/device/water_height_sensor`);
+                res.json({
+                    controls,
+                    phStatus,
+                    nutriStatus,
+                    waterHeightStatus
+                });
+
+
+
+            } catch (error) {
+                console.log(error);
+            }
+
+
         }
 
-        setTimeout(apiCall(), 1000);
+        apiCall();
 
     })
 
